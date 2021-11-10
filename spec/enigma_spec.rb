afc2216ccc8fec87c60cf6c30ffb0159e2986f65
Kevin Gloss <kgloss89@gmail.com>
2021-11-10 14:45:43 -0700

    Update enigma to take no arguments

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -6,7 +6,7 @@ require './lib/enigma'
 RSpec.describe Enigma do
 
 
-  describe 'can read and write to messages' do
+  describe 'can encrypt messages with key and date' do
     before(:all) do
       @enigma = Enigma.new
     end
@@ -15,10 +15,36 @@ RSpec.describe Enigma do
       expect(@enigma).to be_an_instance_of(Enigma)
     end
 
-    xit 'has attributes' do
-      expect(@enigma.message).to eq('hello world')
-      expect(@enigma.key).to eq('')
-      expect(@enigma.date).to eq('')
+    xit '#encrypt' do
+      input = @enigma.encrypt("hello world", "02715", "040895")
+      output = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+        }
+
+      expect(input).to eq(output)
     end
   end
 end
+
+# # decrypt a message with a key and date
+# enigma.decrypt("keder ohulw", "02715", "040895")
+# #=>
+# #   {
+# #     decryption: "hello world",
+# #     key: "02715",
+# #     date: "040895"
+# #   }
+#
+# # encrypt a message with a key (uses today's date)
+# encrypted = enigma.encrypt("hello world", "02715")
+# #=> # encryption hash here
+#
+# #decrypt a message with a key (uses today's date)
+# enigma.decrypt(encrypted[:encryption], "02715")
+# #=> # decryption hash here
+#
+# # encrypt a message (generates random key and uses today's date)
+# enigma.encrypt("hello world")
+# #=> # encryption hash here
