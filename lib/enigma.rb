cd65ad47ed1b444cc6f3a335f6cc35b442be6412
Kevin Gloss <kgloss89@gmail.com>
2021-11-11 11:32:10 -0700

    Feat shifts

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -13,19 +13,27 @@ class Enigma
     rand(99999).to_s.rjust(5, '0')
   end
 
-  def letter_keys(key = random_key)
-    # letters = (:A..:D).to_a
+  def letter_keys(key)
     key_shift = key.chars.each_cons(2).map {|key| key.join.to_i}
-    # Hash[letters.zip(key_shift)]
   end
 
   def date_today
     Date.today.strftime("%d%m%y")
   end
 
-  def letter_offsets(date = date_today)
+  def letter_offsets(date)
     offsets = (date.to_i ** 2).to_s[-4..-1].split(//)
     offsets.map {|offset| offset.to_i}
   end
 
+  def shifts(key, date)
+    letters = (:A..:D).to_a
+    key_offsets = letter_keys(key).zip(letter_offsets(date))
+    shifts = key_offsets.map {|key_offset| key_offset.sum}
+    Hash[letters.zip(shifts)]
+  end
+
+  def encrypt(message, key = random_key, date = date_today)
+
+  end
 end
