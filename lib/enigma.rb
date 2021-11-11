52adef4b94f8707c67ba54044b316b13866fae86
Kevin Gloss <kgloss89@gmail.com>
2021-11-11 11:14:52 -0700

    Feat letter_offsets method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -14,12 +14,18 @@ class Enigma
   end
 
   def letter_keys(key = random_key)
-    letters = (:a..:d).to_a
+    # letters = (:A..:D).to_a
     key_shift = key.chars.each_cons(2).map {|key| key.join.to_i}
-    Hash[letters.zip(key_shift)]
+    # Hash[letters.zip(key_shift)]
   end
 
   def date_today
     Date.today.strftime("%d%m%y")
   end
+
+  def letter_offsets(date = date_today)
+    offsets = (date.to_i ** 2).to_s[-4..-1].split(//)
+    offsets.map {|offset| offset.to_i}
+  end
+
 end
