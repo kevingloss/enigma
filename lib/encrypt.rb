f6f3041fcb3e4c22ca4aa938392ebc15d76d1dee
Kevin Gloss <kgloss89@gmail.com>
2021-11-13 10:01:31 -0700

    Add content to run encryption

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -6,8 +6,8 @@ message = Message.new(ARGV)
 
 #pass arguments into enigma.encrypt
 enigma = Enigma.new
-
+encryption = enigma.encrypt(message.message, message.key, message.date)
 # have message write to translated file
-
+message.write(ARGV[1], encryption[:encryption])
 # print runner summary statement
-puts "Created '#{ARGV[1]}' with the key #{enigma[:key]} and date #{enigma[:date]}"
+puts "Created '#{ARGV[1]}' with the key #{encryption[:key]} and date #{encryption[:date]}"
