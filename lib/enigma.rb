5b1163c1f90dad2d88dab3a0e6154f0c23476b21
Kevin Gloss <kgloss89@gmail.com>
2021-11-14 14:16:38 -0700

    Update encrypt/decrypt to use shift wheel

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -17,18 +17,19 @@ class Enigma
     index = (0..3).to_a
     key_offsets = shift_keys(key).zip(shift_offsets(date))
     shifts = key_offsets.map {|key_offset| key_offset.sum}
-    index.zip(shifts).to_h
+    # index.zip(shifts).to_h
   end
 
-  def shift_wheel(key, date)
-    shifts(key, date).map do |index, shift|
+  def shift_wheel(shifts)
+    shifts.map do |shift|
       @character_set.zip(@character_set.rotate(shift)).to_h
     end
   end
 
   def encrypt(message, key, date)
     message = message.downcase.split(//)
-    shift_wheel = shift_wheel(key, date)
+    shifts = shifts(key, date)
+    shift_wheel = shift_wheel(shifts)
     encrypted_message = encrypt_letters(message, shift_wheel).join
     encrypted_transmission = {
       encryption: encrypted_message,
@@ -52,8 +53,9 @@ class Enigma
 
   def decrypt(message, key, date)
     message = message.downcase.split(//)
-    shifts = shifts(key, date)
-    decrypted_message = decrypt_letters(message, shifts).join
+    shifts = shifts(key, date).map {|shift| shift * -1}
+    shift_wheel = shift_wheel(shifts)
+    decrypted_message = decrypt_letters(message, shift_wheel).join
     decrypted_transmission = {
       decryption: decrypted_message,
       key:  key,
@@ -61,13 +63,13 @@ class Enigma
     }
   end
 
-  def decrypt_letters(message, shifts)
+  def decrypt_letters(message, shift_wheel)
     index = 0
     message.map do |letter|
       if @character_set.include?(letter)
-        decrypted_char = @character_set.zip(@character_set.rotate(-1 * shifts[index%4])).to_h
+        shifted = shift_wheel[index%4]
         index += 1
-        decrypted_char[letter]
+        shifted[letter]
       else
         letter
       end
